452d08da4e8a9fb1feb7b2e32e8b6e420da7f24d
aketchgithub <vickeryace@gmail.com>
2026-01-30 14:53:01 +0300

    All

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import { preprocess } from 'svelte/compiler';
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-vercel';
 
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -13,15 +13,7 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		//adapter: adapter()
-		adapter: adapter({
-            // default options are shown. On some platforms
-            // these options are set automatically — see below
-            pages: 'build',
-            assets: 'build',
-            fallback: 'index.php',
-            precompress: false,
-            strict: true
-        })
+		adapter: adapter()
 	}
 };
 
