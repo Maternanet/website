202086352c991febb1004cc32c38ae37321f45e1
aketchgithub <vickeryace@gmail.com>
2026-01-30 14:38:46 +0300

    All changes

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import { preprocess } from 'svelte/compiler';
 import adapter from '@sveltejs/adapter-static';
 
 
@@ -6,7 +6,7 @@ import adapter from '@sveltejs/adapter-static';
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+	preprocess: preprocess,
 
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
