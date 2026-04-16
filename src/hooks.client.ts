3ce618076922d9699a3a6a70dca4e37614d1e1c1
aketchgithub <vickeryace@gmail.com>
2026-04-16 12:34:45 +0300

    Resolved the error logs.

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -1,9 +1,9 @@
 import posthog from 'posthog-js';
-import { PUBLIC_POSTHOG_PROJECT_TOKEN } from '$env/static/public';
+import { env } from '$env/dynamic/public';
 import type { HandleClientError } from '@sveltejs/kit';
 
 export async function init() {
-	posthog.init(PUBLIC_POSTHOG_PROJECT_TOKEN, {
+	posthog.init(env.PUBLIC_POSTHOG_PROJECT_TOKEN as string, {
 		api_host: '/ingest',
 		ui_host: 'https://us.posthog.com',
 		defaults: '2026-01-30',
